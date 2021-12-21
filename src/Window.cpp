1c715e6fcbc1db534993233aec06858fee381372
galeindor <73552373+galeindor@users.noreply.github.com>
2021-12-21 22:47:39 +0200

    Tried adding my branch

--- src/Window.cpp
+++ src/Window.cpp
@@ -10,10 +10,22 @@ Window::Window()
 
 void Window::run()
 {
+    bool help_opened = false;
     while (m_window.isOpen())
     {
         m_window.clear(sf::Color(179, 218, 255, 255));
         draw();
+        if (help_opened)
+        {
+            auto help_bar = sf::RectangleShape(sf::Vector2f(1134, 735));
+            help_bar.setPosition(sf::Vector2f(300, 50));
+            help_bar.setOutlineThickness(3);
+            help_bar.setOutlineColor(sf::Color::Black);
+            auto texture = sf::Texture();
+            texture.loadFromFile("help.png");
+            help_bar.setTexture(&texture);
+            m_window.draw(help_bar);
+        }
         m_window.display();
 
         for (auto event = sf::Event{}; m_window.pollEvent(event);)
@@ -23,6 +35,20 @@ void Window::run()
             case sf::Event::Closed:
                 m_window.close();
                 break;
+
+            case sf::Event::MouseButtonPressed:
+                auto location = m_window.mapPixelToCoords(
+                    { event.mouseButton.x, event.mouseButton.y });
+
+                switch (event.mouseButton.button)
+                {
+                case sf::Mouse::Button::Left:
+                    help_opened = false;
+                    handleButtons(location, help_opened);
+                    break;
+                }
+                break;
+
             }
         }
     }
@@ -32,4 +58,39 @@ void Window::draw()
 {
     for (int i = 0; i < 3; i++)
         m_buttons[i].draw(m_window);
+}
+
+void Window::handleButtons(const sf::Vector2f& location , bool& help_opened)
+{
+    for (int i = 0; i < MENU_BUTTONS; i++)
+    {
+        if (m_buttons[i].handleClick(location)) // click on 
+        {  
+            switch (i)
+            {
+            case StartGame:
+                // run the game
+                break;
+
+            case Help:
+                help_opened = true;
+                break;
+
+            case Exit:
+                m_window.close();
+                break;
+            default:
+                break;
+            }
+        }
+    }
+}
+
+void Window::open_help()
+{
+    auto help_bar = sf::RectangleShape(sf::Vector2f(841 , 442));
+    help_bar.setPosition(sf::Vector2f(100, 100));
+    auto texture = sf::Texture();
+    texture.loadFromFile("help.png");
+    help_bar.setTexture(&texture);
 }
